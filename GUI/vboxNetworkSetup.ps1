5f17e9bb1d5075264bdf9f0145ebc6d69f48fee2
reg1z <21206554+reg1z@users.noreply.github.com>
2025-03-03 18:59:19 -0800

    Revert "omg it works"
    This reverts commit 3058968d6cc7c18c5b391cdcc355128e89bf6c71.

--- GUI/vboxNetworkSetup.ps1
+++
@@ -1,7 +0,0 @@
-# External Network
-VBoxManage natnetwork add --netname minihackExternal --network "172.20.0.0/16" --enable
-
-# Internal Network
-
-# Create a host-only network interface
-VBoxManage hostonlyif create
