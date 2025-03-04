3058968d6cc7c18c5b391cdcc355128e89bf6c71
reg1z <21206554+reg1z@users.noreply.github.com>
2025-03-03 18:12:45 -0800

    omg it works

---
+++ GUI/vboxNetworkSetup.ps1
@@ -0,0 +1,7 @@
+# External Network
+VBoxManage natnetwork add --netname minihackExternal --network "172.20.0.0/16" --enable
+
+# Internal Network
+
+# Create a host-only network interface
+VBoxManage hostonlyif create
